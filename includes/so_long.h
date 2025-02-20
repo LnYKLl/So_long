068adbb4c45f99c7f59d4525c95283b0851a59fe
LnYKLl <lennykiloul@outlook.com>
2025-02-20 23:51:16 +0100

    fix rollback

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: lkiloul <lkiloul@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 20:16:26 by lkiloul           #+#    #+#             */
-/*   Updated: 2025/02/20 05:20:47 by lkiloul          ###   ########.fr       */
+/*   Updated: 2025/02/20 23:50:25 by lkiloul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,8 +87,6 @@ typedef struct s_game
 int				close_window(t_game *vars);
 int				key_hook(int keycode, t_game *vars);
 int				game_loop(char **argv);
-void			vars_init(t_game *vars);
-
 void			draw_background(t_game *vars, int x, int y);
 void			draw_wall(t_game *vars, int x, int y);
 void			draw_coins(t_game *vars, int x, int y);
@@ -96,8 +94,6 @@ void			draw_map(t_game *vars, char **argv);
 void			draw_exit(t_game *vars, int x, int y);
 void			init_sprites(t_game *vars);
 void			draw_player(t_game *vars, int x, int y);
-void			display_moves(t_game *vars);
-
 int				check_map(t_game *vars, char **argv);
 int				render_map(t_game *vars, char **argv);
 int				map_parsing(t_game *vars, char **argv);
@@ -106,5 +102,6 @@ int				map_read(t_game *vars, char **argv);
 int				map_copy(t_game *vars);
 int				path_alg(t_game *vars, int x, int y);
 int				map_checker(t_game *vars, int fd, char *line, int line_length);
-
+void			display_moves(t_game *vars);
+void	vars_init(t_game *vars);
 #endif
--- srcs/map_check.c
+++ srcs/map_check.c
@@ -6,7 +6,7 @@
 /*   By: lkiloul <lkiloul@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 00:04:23 by lkiloul           #+#    #+#             */
-/*   Updated: 2025/02/20 04:59:28 by lkiloul          ###   ########.fr       */
+/*   Updated: 2025/02/20 01:40:28 by lkiloul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,25 @@
 int	is_char_valid(t_game *vars, char *line, int j)
 {
 	int	i;
+	int	p;
 
 	if (line[0] == '\n' && line[1] == '\0')
 		return (1);
 	if (line[0] != '1' || line[ft_strlen(line) - 2] != '1')
 		return (ft_printf("Error : The map is not surrounded by walls.\n"), 0);
 	i = 0;
+	p = 0;
 	while (line[i])
 	{
 		if (!ft_strchr("10PEC\n", line[i]))
 			return (ft_printf("Error : Invalid character in the map.\n"), 0);
 		if (line[i] == 'P')
 		{
-			if (vars->player.players == 1)
-				return (ft_printf("Error : More than one player in the map.\n"),
+			if (p++)
+				return (perror("Error : More than one player in the map.\n"),
 					0);
 			vars->player.x = i;
 			vars->player.y = j;
-			vars->player.players++;
 		}
 		i++;
 	}
@@ -45,7 +46,7 @@ int	check_file(char **argv)
 {
 	if (ft_strncmp(argv[1] + ft_strlen(argv[1]) - 4, ".ber", 4) != 0)
 	{
-		ft_printf("Error : The file is not a .ber file.\n");
+		perror("Error : The file is not a .ber file.\n");
 		return (0);
 	}
 	return (1);
@@ -53,12 +54,13 @@ int	check_file(char **argv)
 
 int	map_parsing(t_game *vars, char **argv)
 {
-	int		fd;
+	int	fd;
 	char	*line;
 	int		line_length;
-
+	
 	line = NULL;
 	line_length = 0;
+	
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 	{
